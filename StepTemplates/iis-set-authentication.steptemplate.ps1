cacf9c8cc3ba3fceabda73077c73be23a558666d
leslieonline1 <leslieonline1@outlook.com>
2016-06-06 15:48:01 +0100

    Add step template

---
+++ StepTemplates/iis-set-authentication.steptemplate.ps1
@@ -0,0 +1,89 @@
+﻿#requires -version 3
+
+$StepTemplateName = "IIS - Enable or Disable Authentication Methods"
+$StepTemplateDescription = "Step template to set the desired IIS Authentication (Anonymous, Windows, Digest) State for IIS site(s)"
+$StepTemplateParameters = @(
+    @{
+        "Name" = "AnonymousAuth";
+        "Label" = "Anonymous Authentication";
+        "HelpText" = "Enable Anonymous Authentication.";
+        "DefaultValue" = "False";
+        "DisplaySettings" = @{
+            "Octopus.ControlType" = "Checkbox";
+        }
+    },
+    @{
+        "Name" = "WindowsAuth";
+        "Label" = "Windows Authentication";
+        "HelpText" = "Enable Windows Authentication.";
+        "DefaultValue" = "False";
+        "DisplaySettings" = @{
+            "Octopus.ControlType" = "Checkbox";
+        }
+    },
+    @{
+        "Name" = "DigestAuth";
+        "Label" = "Digest Authentication";
+        "HelpText" = "Enable Digest Authentication.";
+        "DefaultValue" = "False";
+        "DisplaySettings" = @{
+            "Octopus.ControlType" = "Checkbox";
+        }
+    },
+    @{
+      "Name" = "IISSitePaths";
+      "Label" = "IIS Site name(s)";
+      "HelpText" = "The IIS site(s) which will have security permissions transformed. Multiple values are to be new-line separated.";
+      "DefaultValue" = $null;
+      "DisplaySettings" = @{
+          "Octopus.ControlType"= "MultiLineText";
+      } 
+    }
+)
+
+function Update-IISSiteAuthentication {
+    param
+    (
+        [ValidateNotNullOrEmpty()]
+        [Parameter(Mandatory = $true)]
+        [boolean]$State,
+        [ValidateNotNullOrEmpty()]
+        [Parameter(Mandatory = $true)]
+        [string]$SitePath,
+        [ValidateNotNullOrEmpty()]
+        [Parameter(Mandatory = $true)]
+        [string]$AuthMethod
+    )
+    
+    # check if WebAdministration module exists on the server
+    $cmd = (Get-Command "Get-Website" -errorAction SilentlyContinue)
+    if ($null -eq $cmd) {
+        throw "The Windows PowerShell snap-in 'WebAdministration' is not installed on this server. Details can be found at https://technet.microsoft.com/en-us/library/ee790599.aspx."
+    }
+    
+    $IISSecurityPath = "/system.WebServer/security/authentication/$AuthMethod"
+    $separator = "`r","`n",","
+    $IISSites = $sitepath.split($separator, [System.StringSplitOptions]::RemoveEmptyEntries).Trim(' ')
+
+    $IISValidSites = Get-Website
+    $IISValidSiteNames = $IISValidSites.Name -join ', '
+
+    foreach($Site in $IISSites) {
+        $IISSiteAvailable = $IISValidSites | Where-Object { $_.Name -eq $Site }
+
+        if ($IISSiteAvailable) {
+            Set-WebConfigurationProperty -Filter $IISSecurityPath -Name Enabled -Value $State -PSPath IIS:\ -Location $Site
+            Write-Output "$AuthMethod for site '$Site' set successfully to '$State'."
+        }
+        else {
+            Write-Output "The IISSitePath '$Site' cannot be found. The valid sites are $IISValidSiteNames"
+            throw "The IISSitePath '$Site' cannot be found. The valid sites are $IISValidSiteNames"
+        }
+    }
+}
+
+if (Test-Path Variable:OctopusParameters) {
+    Update-IISSiteAuthentication -State ($AnonymousAuth -eq "True") -SitePath $IISSitePaths -AuthMethod "AnonymousAuthentication"
+    Update-IISSiteAuthentication -State ($WindowsAuth -eq "True") -SitePath $IISSitePaths -AuthMethod "WindowsAuthentication"
+    Update-IISSiteAuthentication -State ($DigestAuth -eq "True") -SitePath $IISSitePaths -AuthMethod "DigestAuthentication"
+}
---
+++ StepTemplates/iis-set-authentication.steptemplate.tests.ps1
@@ -0,0 +1,61 @@
+﻿$sut = "iis-set-authentication.steptemplate.ps1"
+. "$PSScriptRoot\$sut"
+
+Describe 'IIS - Sets Desired Authentication State' {
+    $AnonymousAuth = "True"
+    $WindowsAuth = "False"
+    $DigestAuth = "False"
+    $IISSitePaths = "Default Web Site/MockApp"
+
+    #WebAdministration module must be installed to run these tests.
+    $cmd = (Get-Command "Get-Website" -errorAction SilentlyContinue)
+    if ($null -eq $cmd) {
+        Get-Module -Name WebAdministration | Remove-Module -Force
+        New-Module -Name WebAdministration  -ScriptBlock {
+            function Get-Website { }
+            Export-ModuleMember -Function Get-Website
+            function Set-WebConfigurationProperty ($Value, $Location, $Filter) { }
+            Export-ModuleMember -Function Set-WebConfigurationProperty
+        } | Import-Module -Force
+    }
+
+    Context "Enabling Authentication" {
+        It "Enables authentication on the site" {
+            Mock Get-Website {return @{Name="Default Web Site/MockApp"; ID="0"; State="Stopped"; PhysicalPath="c:\test"; Bindings="test"}}
+            Mock Set-WebConfigurationProperty
+
+            Update-IISSiteAuthentication -State ($AnonymousAuth -eq "True") -SitePath $IISSitePaths -AuthMethod "AnonymousAuthentication"  
+
+            Assert-MockCalled Get-website -Exactly 1
+            Assert-MockCalled Set-WebConfigurationProperty -Exactly 1 `
+                -ParameterFilter { ($Value -eq $true ) -and ($Location -eq "Default Web Site/MockApp") -and ($Filter -eq "/system.WebServer/security/authentication/AnonymousAuthentication") }
+        }
+    }
+
+    Context "Authentication Update Failed" {
+        It "Authentication fails to update" {
+            Mock Get-Website {return @{Name="Default Web Site/MockApp"; ID="0"; State="Stopped"; PhysicalPath="c:\test"; Bindings="test"}}
+            Mock Set-WebConfigurationProperty { throw "A fake error test message"}
+                
+            { Update-IISSiteAuthentication -State ($DigestAuth -eq "True") -SitePath $IISSitePaths -AuthMethod "DigestAuthentication"  } | should throw
+
+            Assert-MockCalled Get-website -Exactly 1
+            Assert-MockCalled Set-WebConfigurationProperty -Exactly 1 `
+                -ParameterFilter { ($Value -eq $false ) -and ($Location -eq "Default Web Site/MockApp") -and ($Filter -eq "/system.WebServer/security/authentication/DigestAuthentication") }
+        }
+    }
+
+    Context "Incorrect SitePath" {
+        $IISSitePaths = "Default Web Site/DoesNotExist"
+
+        It "IIS Site cannot be found" {
+            Mock Get-Website {return @{Name=""; ID=""; State=""; PhysicalPath=""; Bindings=""}}
+            Mock Set-WebConfigurationProperty
+
+            { Update-IISSiteAuthentication -State ($WindowsAuth -eq "True") -SitePath $IISSitePaths -AuthMethod "WindowsAuthentication"  } | should throw
+
+            Assert-MockCalled Get-website -Exactly 1
+            Assert-MockCalled Set-WebConfigurationProperty -Exactly 0
+        }
+    }
+}
